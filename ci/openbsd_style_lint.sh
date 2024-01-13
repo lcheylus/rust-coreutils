7be6c5e6a542ab2ad106f872147be1014aeb00ea
Laurent Cheylus <foxy@free.fr>
2024-01-13 23:49:40 +0100

    OpenBSD workflow: enable job for Style and Lint

--- ci/openbsd_style_lint.sh
+++ ci/openbsd_style_lint.sh
@@ -51,21 +51,32 @@ printf "\n## tooling info\n"
 rustc -V
 cargo -V
 
-#
 # To ensure that files are cleaned up, we don't want to exit on error
-# set +e
-# unset FAULT
-# ## cargo fmt testing
-# echo "## cargo fmt testing"
-# # * convert any errors/warnings to GHA UI annotations; ref: <https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message>
-# S=\$(cargo fmt -- --check) && printf "%s\n" "\$S" || { printf "%s\n" "\$S" ; printf "%s\n" "\$S" | sed -E -n -e "s/^Diff[[:space:]]+in[[:space:]]+\${PWD//\//\\\\/}\/(.*)[[:space:]]+at[[:space:]]+[^0-9]+([0-9]+).*\$/::\${FAULT_TYPE} file=\1,line=\2::\${FAULT_PREFIX}: \\\`cargo fmt\\\`: style violation (file:'\1', line:\2; use \\\`cargo fmt -- \"\1\"\\\`)/p" ; FAULT=true ; }
-# ## cargo clippy lint testing
-# if [ -z "\${FAULT}" ]; then
-# echo "## cargo clippy lint testing"
-# # * convert any warnings to GHA UI annotations; ref: <https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message>
-# S=\$(cargo clippy --all-targets \${CARGO_UTILITY_LIST_OPTIONS} -- -W clippy::manual_string_new -D warnings 2>&1) && printf "%s\n" "\$S" || { printf "%s\n" "\$S" ; printf "%s" "\$S" | sed -E -n -e '/^error:/{' -e "N; s/^error:[[:space:]]+(.*)\\n[[:space:]]+-->[[:space:]]+(.*):([0-9]+):([0-9]+).*\$/::\${FAULT_TYPE} file=\2,line=\3,col=\4::\${FAULT_PREFIX}: \\\`cargo clippy\\\`: \1 (file:'\2', line:\3)/p;" -e '}' ; FAULT=true ; }
-# fi
-# # Clean to avoid to rsync back the files
-# cargo clean
-# if [ -n "\${FAIL_ON_FAULT}" ] && [ -n "\${FAULT}" ]; then exit 1 ; fi
-#
+set +e
+unset FAULT
+
+## cargo fmt testing
+echo "## cargo fmt testing"
+# * convert any errors/warnings to GHA UI annotations; ref: <https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message>
+S=$(cargo fmt -- --check) && printf "%s\n" "$S" || {
+    printf "%s\n" "$S"
+    printf "%s\n" "$S" | sed -E -n -e "s/^Diff[[:space:]]+in[[:space:]]+${PWD//\//\\/}\/(.*)[[:space:]]+at[[:space:]]+[^0-9]+([0-9]+).*$/::${FAULT_TYPE} file=\1,line=\2::${FAULT_PREFIX}: \`cargo fmt\`: style violation (file:'\1', line:\2; use \`cargo fmt -- \"\1\"\`)/p"
+    FAULT=true
+}
+
+## cargo clippy lint testing
+if [ -z "${FAULT}" ]; then
+    echo "## cargo clippy lint testing"
+    # * convert any warnings to GHA UI annotations; ref: <https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message>
+    S=$(cargo clippy --all-targets ${CARGO_UTILITY_LIST_OPTIONS} -- -W clippy::manual_string_new -D warnings 2>&1) && printf "%s\n" "$S" || {
+        printf "%s\n" "$S"
+        printf "%s" "$S" | sed -E -n -e '/^error:/{' -e "N; s/^error:[[:space:]]+(.*)\\n[[:space:]]+-->[[:space:]]+(.*):([0-9]+):([0-9]+).*$/::${FAULT_TYPE} file=\2,line=\3,col=\4::${FAULT_PREFIX}: \`cargo clippy\`: \1 (file:'\2', line:\3)/p;" -e '}'
+        FAULT=true
+    }
+fi
+
+# Clean to avoid to rsync back the files
+cargo clean
+if [ -n "${FAIL_ON_FAULT}" ] && [ -n "${FAULT}" ]; then
+    exit 1
+fi
