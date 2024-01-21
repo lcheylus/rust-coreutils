0b462f55ebfaf7afe26ce4001698052fe681247e
Laurent Cheylus <foxy@free.fr>
2024-01-21 08:25:00 +0100

    tests: fix warnings for unused imports in by-util/test_* on OpenBSD
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- tests/by-util/test_uptime.rs
+++ tests/by-util/test_uptime.rs
@@ -3,6 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 use crate::common::util::TestScenario;
+#[cfg(not(target_os = "openbsd"))]
 use regex::Regex;
 
 #[test]
