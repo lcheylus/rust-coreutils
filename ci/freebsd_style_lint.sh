6027a9c3d92fbac09b9edd45a3f25760ab6f0b54
Laurent Cheylus <foxy@free.fr>
2024-04-07 12:53:35 +0200

    CI: improve output for Rust infos in BSD scripts
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- ci/freebsd_style_lint.sh
+++ ci/freebsd_style_lint.sh
@@ -54,9 +54,10 @@ echo "FAULT_PREFIX='${FAULT_PREFIX}'"
 echo "UTILITY_LIST='${UTILITY_LIST}'"
 env | sort
 
-# tooling info
-printf "\n## tooling info\n"
+# Rust tools infos
+printf "\n## Rust infos\n"
 rustc -vV
+printf "\n## cargo infos\n"
 cargo -vV
 
 unset FAULT
