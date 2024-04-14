0532c0c11b39743fb3f8c113f6f432364d9c33d4
Laurent Cheylus <foxy@free.fr>
2024-04-14 18:11:58 +0200

    CI: update Rust version 1.77.2 in fake script for FreeBSD
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- ci/fake_rustc_freebsd-stable.sh
+++ ci/fake_rustc_freebsd-stable.sh
@@ -5,11 +5,11 @@
 #
 
 cat << EOF
-rustc 1.77.1 (7cf61ebde 2024-03-27)
+rustc 1.77.2 (25ef9e3d8 2024-04-09)
 binary: rustc
-commit-hash: 7cf61ebde7b22796c69757901dd346d0fe70bd97
-commit-date: 2024-03-27
+commit-hash: 25ef9e3d85d934b27d9dada2f9dd52b1dc63bb04
+commit-date: 2024-04-09
 host: x86_64-unknown-freebsd
-release: 1.77.1
+release: 1.77.2
 LLVM version: 17.0.6
 EOF
