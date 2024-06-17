c50d71e7adf7f1a6cf61ed8b92823e831ca77100
Laurent Cheylus <foxy@free.fr>
2024-06-17 12:28:01 +0200

    tests: disable failed tests for cp on OpenBSD
    Disable tests for cp on OpenBSD with permissions
    Tests added in commit e46ad3043439b45454389be49f754c5923a9b4b8 (17/06/2024)
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- tests/by-util/test_cp.rs
+++ tests/by-util/test_cp.rs
@@ -5748,6 +5748,7 @@ fn test_preserve_attrs_overriding_2() {
 /// Test the behavior of preserving permissions when copying through a symlink
 #[test]
 #[cfg(unix)]
+#[cfg(not(target_os = "openbsd"))]
 fn test_cp_symlink_permissions() {
     let scene = TestScenario::new(util_name!());
     let at = &scene.fixtures;
@@ -5770,6 +5771,7 @@ fn test_cp_symlink_permissions() {
 /// Test the behavior of preserving permissions of parents when copying through a symlink
 #[test]
 #[cfg(unix)]
+#[cfg(not(target_os = "openbsd"))]
 fn test_cp_parents_symlink_permissions_file() {
     let scene = TestScenario::new(util_name!());
     let at = &scene.fixtures;
@@ -5794,6 +5796,7 @@ fn test_cp_parents_symlink_permissions_file() {
 /// a symlink when source is a dir.
 #[test]
 #[cfg(unix)]
+#[cfg(not(target_os = "openbsd"))]
 fn test_cp_parents_symlink_permissions_dir() {
     let scene = TestScenario::new(util_name!());
     let at = &scene.fixtures;
