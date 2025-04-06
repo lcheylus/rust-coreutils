22d41e5c31afde13abbed0541bd975a9dc60077b
Laurent Cheylus <foxy@free.fr>
2025-04-06 10:13:52 +0200

    CI: update Rust version 1.86.0 in fake script for FreeBSD
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- ci/fake_rustc_freebsd-stable.sh
+++ ci/fake_rustc_freebsd-stable.sh
@@ -5,11 +5,11 @@
 #
 
 cat << EOF
-rustc 1.85.1 (4eb161250 2025-03-15)
+rustc 1.86.0 (05f9846f8 2025-03-31)
 binary: rustc
-commit-hash: 4eb161250e340c8f48f66e2b929ef4a5bed7c181
-commit-date: 2025-03-15
+commit-hash: 05f9846f893b09a1be1fc8560e33fc3c815cfecb
+commit-date: 2025-03-31
 host: x86_64-unknown-freebsd
-release: 1.85.1
+release: 1.86.0
 LLVM version: 19.1.7
 EOF
