5486f2f96a197442c270cc1a05ac0cf623adcb0e
Laurent Cheylus <foxy@free.fr>
2025-11-08 18:10:53 +0100

    stat: fix warning on OpenBSD
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

--- tests/by-util/test_stat.rs
+++ tests/by-util/test_stat.rs
@@ -3,6 +3,7 @@
 // For the full copyright and license information, please view the LICENSE
 // file that was distributed with this source code.
 
+#[cfg(not(target_os = "openbsd"))]
 use uutests::at_and_ucmd;
 use uutests::new_ucmd;
 use uutests::unwrap_or_return;
