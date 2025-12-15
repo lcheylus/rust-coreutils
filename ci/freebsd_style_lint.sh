23ee5bee89931875da83aff44ee24ef67bfd8d8b
Laurent Cheylus <foxy@free.fr>
2025-12-15 18:48:57 +0100

    CI: use shell scripts for FreeBSD style/tests jobs
    In FreeBSD CI, shell scripts are executed to run "Style and Lint" and "Tests" jobs
    - ci/freebsd_style_lint.sh
    - ci/freebsd_tests.sh
    
    At the end of the "Tests" job, a report (Markdown format) is generated with list of
    succeeded/skipped/failed tests.
    
    Signed-off-by: Laurent Cheylus <foxy@free.fr>

---
+++ ci/freebsd_style_lint.sh
@@ -0,0 +1,88 @@
+#!/usr/bin/env bash
+#
+# Bash script to run "Style and Lint" job (GH workflow) on FreeBSD
+#
+
+set -e
+
+# First, we check that this script is not run as root because it would fail tests.
+if [ "root" == "$(whoami)" ]; then
+    echo "ERROR: don't run this script as root"
+    exit 1;
+fi
+
+## Infos
+echo "## OS infos"
+uname -a
+
+echo "## user infos"
+userinfo "$(id -u)"
+
+# Install rust-stable via rustup
+curl https://sh.rustup.rs -sSf --output rustup.sh
+sh rustup.sh -y -c rustfmt,clippy --profile=minimal -t stable
+. ${HOME}/.cargo/env
+
+## VARs setup
+cd "${WORKSPACE}"
+
+unset FAIL_ON_FAULT
+case "${STYLE_FAIL_ON_FAULT}" in
+''|0|f|false|n|no|off)
+    FAULT_TYPE=warning ;;
+*)
+    FAIL_ON_FAULT=true
+    FAULT_TYPE=error ;;
+esac;
+
+FAULT_PREFIX=$(echo "${FAULT_TYPE}" | tr '[:lower:]' '[:upper:]')
+
+# determine sub-crate utility list
+UTILITY_LIST="$(./util/show-utils.sh --features "${FEATURES}")"
+CARGO_UTILITY_LIST_OPTIONS="$(for u in ${UTILITY_LIST}; do echo -n "-puu_${u} "; done;)"
+
+# environment
+echo "## environment"
+echo "CI='${CI}'"
+echo "REPO_NAME='${REPO_NAME}'"
+echo "WORKSPACE_PARENT='${WORKSPACE_PARENT}'"
+echo "WORKSPACE='${WORKSPACE}'"
+echo "FAULT_PREFIX='${FAULT_PREFIX}'"
+echo "UTILITY_LIST='${UTILITY_LIST}'"
+env | sort
+
+# tooling info
+printf "\n## tooling info\n"
+rustc -vV
+cargo -vV
+
+# To ensure that files are cleaned up, we don't want to exit on error
+set +e
+unset FAULT
+
+## cargo fmt testing
+printf "\n## cargo fmt testing\n"
+
+# * convert any errors/warnings to GHA UI annotations; ref: <https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message>
+S=$(cargo fmt -- --check) && printf "%s\n" "$S" || {
+    printf "%s\n" "$S"
+    printf "%s\n" "$S" | sed -E -n -e "s/^Diff[[:space:]]+in[[:space:]]+${PWD//\//\\/}\/(.*)[[:space:]]+at[[:space:]]+[^0-9]+([0-9]+).*$/::${FAULT_TYPE} file=\1,line=\2::${FAULT_PREFIX}: \`cargo fmt\`: style violation (file:'\1', line:\2; use \`cargo fmt -- \"\1\"\`)/p"
+    FAULT=true
+}
+
+## cargo clippy lint testing
+if [ -z "${FAULT}" ]; then
+    echo "## cargo clippy lint testing"
+    # * convert any warnings to GHA UI annotations; ref: <https://help.github.com/en/actions/reference/workflow-commands-for-github-actions#setting-a-warning-message>
+    S=$(cargo clippy --all-targets ${CARGO_UTILITY_LIST_OPTIONS} -- -D warnings 2>&1) && printf "%s\n" "$S" || {
+        printf "%s\n" "$S"
+        printf "%s" "$S" | sed -E -n -e '/^error:/{' -e "N; s/^error:[[:space:]]+(.*)\\n[[:space:]]+-->[[:space:]]+(.*):([0-9]+):([0-9]+).*$/::${FAULT_TYPE} file=\2,line=\3,col=\4::${FAULT_PREFIX}: \`cargo clippy\`: \1 (file:'\2', line:\3)/p;" -e '}'
+        FAULT=true
+    }
+fi
+
+# Clean to avoid to rsync back the files
+cargo clean
+if [ -n "${FAIL_ON_FAULT}" ] && [ -n "${FAULT}" ]; then
+    exit 1
+fi
---
+++ ci/freebsd_tests.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+#
+# Bash script to run Tests job (GH workflow) on FreeBSD
+#
+
+set -e
+
+# First, we check that this script is not run as root because it would fail tests.
+if [ "root" == "$(whoami)" ]; then
+    echo "ERROR: don't run this script as root"
+    exit 1;
+fi
+
+## Infos
+echo "## OS infos"
+uname -a
+
+echo "## user infos"
+userinfo "$(id -u)"
+
+echo "## hostname"
+hostname
+
+# Install rust-stable via rustup
+curl https://sh.rustup.rs -sSf --output rustup.sh
+sh rustup.sh -y -c rustfmt,clippy --profile=minimal -t stable
+. ${HOME}/.cargo/env
+
+# Install nextest
+mkdir -p ~/.cargo/bin
+curl -LsSf https://get.nexte.st/latest/freebsd | tar zxf - -C ~/.cargo/bin
+
+# environment
+echo "## environment"
+echo "CI='${CI}'"
+echo "REPO_NAME='${REPO_NAME}'"
+echo "WORKSPACE_PARENT='${WORKSPACE_PARENT}'"
+echo "WORKSPACE='${WORKSPACE}'"
+env | sort
+
+# tooling info
+printf "\n## tooling info\n"
+rustc -vV
+cargo -vV
+cargo nextest --version
+
+cd "${WORKSPACE}"
+rm -f tests-ok
+unset FAULT
+export CARGO_TERM_COLOR=always
+
+cargo build || FAULT=1
+
+export RUST_BACKTRACE=1
+
+if test -z "$FAULT"; then
+    # cargo nextest run --hide-progress-bar --profile ci --features "${FEATURES}" || FAULT=1
+    NEXTEST_EXPERIMENTAL_LIBTEST_JSON=1 cargo nextest run --hide-progress-bar --profile ci --features "${FEATURES}" --message-format libtest-json-plus 1> "${WORKSPACE}/tests-unix.json" || FAULT=1
+fi
+# There is no systemd-logind on FreeBSD, so test all features except feat_systemd_logind ( https://github.com/rust-lang/cargo/issues/3126#issuecomment-2523441905 )
+if test -z "$FAULT"; then
+    # cargo nextest run --hide-progress-bar --profile ci --features "\$UUCORE_FEATURES" -p uucore || FAULT=1
+    UUCORE_FEATURES=$(cargo metadata --format-version=1 --no-deps | jq -r '.packages[] | select(.name == "uucore") | .features | keys | .[]' | grep -v "feat_systemd_logind" | paste -s -d "," -)
+    NEXTEST_EXPERIMENTAL_LIBTEST_JSON=1 cargo nextest run --hide-progress-bar --profile ci --features "${UUCORE_FEATURES}" -p uucore --message-format libtest-json-plus 1> "${WORKSPACE}/tests-uucore.json" || FAULT=1
+fi
+
+# Clean to avoid to rsync back the files
+cargo clean
+
+# Do not exit for shell with return code 1 => prevent further execution of GH workflow
+if test -n "$FAULT"; then
+    exit 0
+else
+    touch "${WORKSPACE}"/tests-ok
+    exit 0
+fi
